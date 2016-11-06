0f1cfdc2b1c053750c4fe3b7bf157faea4e361ff
jamie <272915189@qq.com>
2016-11-06 23:48:10 +0800

    First commit.

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+/**
+* @flow
+*/
+
+import {
+  AppRegistry
+} from 'react-native';
+
+import {
+  NativeNavigator,
+} from './NativeNavigator'
+
+class SoloOneself extends NativeNavigator {
+
+}
+
+AppRegistry.registerComponent('SoloOneself', () => SoloOneself);
---
+++ platform_independent_components/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports.SampleAppMovies = require('./SampleAppMovies').SampleAppMovies;
+module.exports.SampleMovieDetail = require('./SampleMovieDetail').SampleMovieDetail;
